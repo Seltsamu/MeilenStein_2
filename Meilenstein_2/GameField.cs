15f6eb06ecb4cbc8a64e98c2aaa059e98eb47021
Samuel Seltmann <seltsam21@online.de>
2025-05-04 13:32:41 +0200

    add start method, add move mechanics and special field interactions

--- Meilenstein_2/GameField.cs
+++ Meilenstein_2/GameField.cs
@@ -13,11 +13,19 @@ public class GameField
         public int Number = number;
     }
 
-    internal class Player(string name)
+    internal class Player(string name, FieldNode? position)
     {
         public string Name = name;
         public int Throws = 0;
-        public FieldNode? Position;
+        public FieldNode? Position = position;
+        private static int _nextNum = 1;
+        public int Num = _nextNum++;
+        public bool Winner = false;
+
+        public override string ToString()
+        {
+            return $"Player{Num}({Name})";
+        }
     }
 
     private FieldNode? _first;
@@ -30,47 +38,52 @@ public class GameField
     {
         _fieldCount = length;
         CreateFieldNodes();
-        _player1 = new Player(name1);
-        _player2 = new Player(name2);
+        _player1 = new Player(name1, _first);
+        _player2 = new Player(name2, _first);
     }
 
     private void CreateFieldNodes()
     {
         if (_fieldCount <= 6)
             throw new ArgumentException("Game field must be larger than 6 fields!");
-        if (_fieldCount > 200)
+        if (_fieldCount > 300)
             throw new ArgumentException("GameField must be smaller than 201 fields!");
 
-        int number = 1;
-        _first = new FieldNode(null, null, number++);
+        _first = new FieldNode(null, null, 1);
         _last = new FieldNode(_first, null, _fieldCount);
         _first.Next = _last;
 
         for (int i = 0; i < _fieldCount - 2; i++) // create FieldNodes with a random chance for snake or ladder
         {
-            FieldNode newFieldNode;
-            Random random = new Random();
-            int num = random.Next(5); // TODO create boundary's for how many snakes and letters can be in a row
-
-            if (num == 1) // with snake
-            {
-                newFieldNode = new FieldNode(_last.Prev, _last, number++, true);
-            }
-            else if (num == 2) // with ladder
-            {
-                newFieldNode = new FieldNode(_last.Prev, _last, number++, false, true);
-            }
-            else // without anything special
-            {
-                newFieldNode = new FieldNode(_last.Prev, _last, number++);
-            }
-
-            _last.Prev!.Next = newFieldNode;
-            _last.Prev = newFieldNode;
+            AddFieldNodes();
         }
     }
 
-    public void PrintGameField() // TODO make it colorful and find a way to use this method
+    private void AddFieldNodes()
+    {
+        FieldNode newFieldNode;
+        Random random = new Random();
+        int num = random.Next(5); // TODO create boundary's for how many snakes and letters can be in a row
+
+        if (num == 1) // with snake
+        {
+            newFieldNode = new FieldNode(_last!.Prev, _last, _last.Number - 1, true);
+        }
+        else if (num == 2) // with ladder
+        {
+            newFieldNode = new FieldNode(_last!.Prev, _last, _last.Number - 1, false, true);
+        }
+        else // without anything special
+        {
+            newFieldNode = new FieldNode(_last!.Prev, _last, _last.Number - 1);
+        }
+
+        _last.Prev!.Next = newFieldNode;
+        _last.Prev = newFieldNode;
+        _last.Number += 1;
+    }
+
+    public void PrintGameField() // TODO make it colorful
     {
         Console.OutputEncoding = Encoding.UTF8;
 
@@ -103,7 +116,7 @@ public class GameField
             }
         }
 
-        for (int i = 0; i < height; i++)
+        for (int i = 0; i < height; i++) // print the Game field with all symbols
         {
             int length;
             if (i == 0)
@@ -128,6 +141,8 @@ public class GameField
                     Console.Write($"│{p}L │");
                 else if (fieldNodes[i, j]!.Snake)
                     Console.Write($"│{p}S │");
+                else if (_player1.Position == _first && _player2.Position == _first && fieldNodes[i, j] == _first)
+                    Console.Write($"│1 2│");
                 else
                     Console.Write($"│ {p} │");
             }
@@ -140,5 +155,82 @@ public class GameField
 
             Console.WriteLine();
         }
+    }
+
+    public void Start()
+    {
+        Player currentPlayer = _player2;
+        while (!currentPlayer.Winner)
+        {
+            currentPlayer = currentPlayer == _player1 ? _player2 : _player1; // switch between player1 and player2
+            Console.Clear();
+            PrintGameField();
+
+            Console.WriteLine($"{currentPlayer} is on the move. Press enter to roll the dice");
+            Console.ReadKey();
+            
+            int turn = RollDice();
+            Console.WriteLine($"You rolled a: {turn}. Press enter to make your turn");
+            Console.ReadKey();
+            
+            MakeTurn(currentPlayer, turn);
+            
+            
+        }
+    }
+
+    private void MakeTurn(Player player, int turn)
+    {
+        if (turn == 1)
+        {
+            IncreaseGameField();
+        }
+        
+        MovePlayer(player, turn);
+        
+        if (player.Position!.Ladder)
+        {
+            Console.WriteLine("You land on a Ladder. Move an extra 3 fields forward");
+            MovePlayer(player, 3);
+        }
+        else if (player.Position!.Snake)
+        {
+            Console.WriteLine("You land on a Snake. Move 3 fields back");
+            for (int i = 0; i < 3; i++)
+            {
+                player.Position = player.Position!.Prev;
+                if (player.Position == _first)
+                    break;
+            }
+        }  
+        
+        if (_player1.Position == _player2.Position && _player1.Position != _first)
+            player.Position = player.Position!.Prev;
+    }
+
+    private void IncreaseGameField()
+    {
+        _fieldCount += 5;
+        for (int i = 0; i < 5; i++)
+        {
+            AddFieldNodes(); // TODO weis nicht obs stimmt
+        }
+    }
+
+    private void MovePlayer(Player player, int num)
+    {
+        for (int i = 0; i < num; i++)
+        {
+            player.Position = player.Position!.Next;
+
+            if (player.Position == _last)
+                player.Winner = true;
+        }
+    }
+
+    private static int RollDice()
+    {
+        Random random = new Random();
+        return random.Next(1, 7);
     }
 }
